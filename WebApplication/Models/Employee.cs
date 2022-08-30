466bf9f6fc8db244b5f1493b5add4d914fb29381
yimon3 <scm.yimonoo@gmail.com>
2022-08-30 23:17:17 +0630

    Add project files.

---
+++ WebApplication/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    /// <summary>
+    /// Define employee class
+    /// </summary>
+    public class Employee
+    {
+        public int employeeId { get; set; }
+
+        public string employeeName { get; set; }
+
+        public string department { get; set; }
+
+        public DateTime joining_date { get; set; }
+
+        public string upload_image { get; set; }
+    }
+}
